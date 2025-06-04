3c99b87c19a3a1cf6d6eec4461161ac3e180de65
rahulsharma8848 <rahul.sharma@8848digital.com>
2025-06-04 20:50:01 +0530

    update

--- controlz_customizations/apis/clickpost_order.py
+++ controlz_customizations/apis/clickpost_order.py
@@ -1,5 +1,7 @@
 import frappe
 from frappe.model.mapper import get_mapped_doc
+from frappe.utils import now
+import requests
 
 @frappe.whitelist()
 def make_clickpost_doc_from_sales_order_to_customer(source_name,target_doc=None):
@@ -17,6 +19,20 @@ def make_clickpost_doc_from_sales_order_to_customer(source_name,target_doc=None)
         target.pickup_pincode = address.pincode
         target.pickup_email = customer.cu_email
         target.pickup_phone = customer.cu_mobile_number
+        target.pickup_name = so.customer_name
+        target.pickup_time= now()
+        target.pickup_district= get_district_from_pincode_and_address(so.customer_address)
+        target.invoice_number=so.name
+        target.reference_number=so.name
+        target.order_type=so.custom_order_payment_type
+        if so.items:
+            first_item = so.items[0]
+            target.append("clickpost_shipment_item", {
+                "sku": first_item.item_code,
+                "quantity": first_item.qty,
+                "description": first_item.item_code,
+                "price": first_item.price_list_rate,
+            })
 
     doclist = get_mapped_doc("Sales Order", source_name,
     {
@@ -25,7 +41,17 @@ def make_clickpost_doc_from_sales_order_to_customer(source_name,target_doc=None)
             "field_map": {
             }
         },
+        
 
     }, target_doc,set_missing_values)
 
     return doclist
+
+def get_district_from_pincode_and_address(customer_address):
+    address=frappe.get_doc("Address",customer_address)
+    pincode=address.pincode
+    response = requests.get(f"https://api.postalpincode.in/pincode/{pincode}")
+    data = response.json()
+    if data[0]['Status'] == 'Success':
+        return data[0]['PostOffice'][0]['District']
+    return None
