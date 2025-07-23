4c68dda9b2dc25d75381d35e259ae575736d8051
Jigar Tarpara <jigartarpara@khatavahi.in>
2025-07-23 16:37:51 +0530

    Changes

--- controlz_customizations/controlz_customizations/report/manufacturing_cost_report/manufacturing_cost_report.py
+++ controlz_customizations/controlz_customizations/report/manufacturing_cost_report/manufacturing_cost_report.py
@@ -8,10 +8,12 @@ def execute(filters=None):
 	columns = [
         {"label": "Stock Entry", "fieldname": "stock_entry", "fieldtype": "Link", "options": "Stock Entry", "width": 300},
         {"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date", "width": 350},
+		{"label": "Serial No", "fieldname": "serial_no", "fieldtype": "Link", "options": "Serial No","width": 180},
         {"label": "FG Item", "fieldname": "fg_item", "fieldtype": "Link", "options": "Item", "width": 300},
+        {"label": "FG Seri", "fieldname": "fg_item", "fieldtype": "Link", "options": "Item", "width": 300},
         {"label": "FG Value", "fieldname": "fg_value", "fieldtype": "Float", "width": 180},
         {"label": "Part Value", "fieldname": "part_value", "fieldtype": "Float", "width": 180},
-        {"label": "Scrap Value", "fieldname": "scrap_value", "fieldtype": "Float", "width": 180},
+        
     ]
 	data = get_data(filters)
 	return columns, data
@@ -33,15 +35,17 @@ def get_data(filters):
 		fg_item = ""
 		fg_value = 0.0
 		scrap_value = 0.0
+		serial_no = ""
 		for item in stock_entry.items:
-			if item.idx != 1:
-				if item.is_finished_item:
-					fg_item = item.item_code
-					fg_value = item.basic_rate
-				elif item.is_scrap_item:
-					scrap_value = item.basic_rate
-				else:
-					part_value += item.basic_rate
+			if item.idx == 1:
+				serial_no = item.serial_no
+			elif item.is_finished_item:
+				fg_item = item.item_code
+				fg_value = item.basic_rate
+			elif item.is_scrap_item:
+				scrap_value = item.basic_rate
+			else:
+				part_value += item.basic_rate
 
 
 		final_output.append({
@@ -50,7 +54,8 @@ def get_data(filters):
 			"fg_item": fg_item,
 			"fg_value": fg_value,
 			"part_value": part_value,
-			"scrap_value": scrap_value
+			"scrap_value": scrap_value,
+			"serial_no": serial_no
 		})
 
 	return final_output
