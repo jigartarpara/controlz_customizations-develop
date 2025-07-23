80ea833bee0e462cfb338e32c5181188fed4167e
Jigar Tarpara <jigartarpara@khatavahi.in>
2025-07-23 16:41:51 +0530

    Changes

--- controlz_customizations/controlz_customizations/report/manufacturing_cost_report/manufacturing_cost_report.py
+++ controlz_customizations/controlz_customizations/report/manufacturing_cost_report/manufacturing_cost_report.py
@@ -10,9 +10,9 @@ def execute(filters=None):
         {"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date", "width": 350},
 		{"label": "Serial No", "fieldname": "serial_no", "fieldtype": "Link", "options": "Serial No","width": 180},
         {"label": "FG Item", "fieldname": "fg_item", "fieldtype": "Link", "options": "Item", "width": 300},
-        {"label": "FG Seri", "fieldname": "fg_item", "fieldtype": "Link", "options": "Item", "width": 300},
         {"label": "FG Value", "fieldname": "fg_value", "fieldtype": "Float", "width": 180},
         {"label": "Part Value", "fieldname": "part_value", "fieldtype": "Float", "width": 180},
+        {"label": "Scrap Value", "fieldname": "scrap_value", "fieldtype": "Float", "width": 300},
         
     ]
 	data = get_data(filters)
